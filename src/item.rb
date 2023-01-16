174010debbc2faa42a3a413204375b9b07471595
recillah khamala <recillahk@gmail.com>
2023-01-16 16:34:41 +0300

    Implement methods

--- src/item.rb
+++ src/item.rb
@@ -1,2 +1,43 @@
 class Item
+    attr_accessor :publish_date, :id, :archived, :labels, :authors, :genres
+
+    def initialize(publish_date, id: rand(1..100), archived: false)
+      @id = id
+      @genres = nil
+      @authors = nil
+      @labels = nil
+      @source = nil
+      @publish_date = publish_date
+      @archived = archived
+    end
+
+    def genres
+        @genres = genres
+    end
+
+    def authors
+        @authors = authors
+    end
+
+    def labels
+        @labels = labels
+    end
+
+    def source
+        @source = source
+    end
+
+    def can_be_archived?
+        if Time.now.year - @publish_date.year > 10
+            return true
+        else
+           return false 
+        end
+    end
+
+    def move_to_archive
+        @archived = can_be_archived?
+    end
+
 end
+
