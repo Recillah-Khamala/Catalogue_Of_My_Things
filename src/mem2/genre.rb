cdd069a7d97d3d9c9fbe21475436038f87ed8379
Kaweesi Matia <matiakaweesi@gmail.com>
2023-01-17 10:50:10 +0300

    add genre.rb and music_album.rb files

--- src/mem2/genre.rb
+++ src/mem2/genre.rb
@@ -11,3 +11,17 @@
 # All data should be preserved by saving collections in .json files.
 # Create a schema.sql file with tables that will be analogical to the structure of the classes that you created:
 # genres table
+class Genre
+  attr_reader :items, :name, :id
+
+  def initialize(name, id: rand(1..1000))
+    @id = id
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.genres = self
+  end
+end
