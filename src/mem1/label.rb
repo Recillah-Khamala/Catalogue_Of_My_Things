5bf1e273fbbe16ed6dad8751faf3ce1e9caf8c2e
recillah khamala <recillahk@gmail.com>
2023-01-18 16:48:13 +0300

    Implement add_item method

--- src/mem1/label.rb
+++ src/mem1/label.rb
@@ -11,13 +11,23 @@ class Label
     @color = color
     @items = []
   end
+
+  # Implement methods:
+  #   add_item method in the Label class
+  #     should take an instance of the Item class as an input
+  #     should add the input item to the collection of items
+  #     should add self as a property of the item object (by using the correct setter from the item object)
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
 
 # Implement methods:
-# add_item method in the Label class
-# should take an instance of the Item class as an input
-# should add the input item to the collection of items
-# should add self as a property of the item object (by using the correct setter from the item object)
+#   add_item method in the Label class
+#     should take an instance of the Item class as an input
+#     should add the input item to the collection of items
+#     should add self as a property of the item object (by using the correct setter from the item object)
 # Add unit tests for all implemented methods.
 # The following options should be available:
 # List all labels (e.g. 'Gift', 'New')
