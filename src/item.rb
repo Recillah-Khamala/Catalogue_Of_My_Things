ea101097fd2afa36e29c83f37ab3fda83e5cac85
tashisrael <tashisrael04@gmail.com>
2023-01-20 15:33:33 +0300

    review changes

--- src/item.rb
+++ src/item.rb
@@ -1,6 +1,7 @@
 require 'date'
 class Item
-  attr_accessor :id, :publish_date, :archived, :label, :author, :genre
+  attr_accessor :id, :publish_date, :archived
+  attr_reader :label, :author, :genre
 
   def initialize(publish_date, archived)
     @id = Random.rand(1..100)
@@ -15,5 +16,20 @@ class Item
 
   def move_to_archive
     @archived = true if can_be_archived?
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 end
